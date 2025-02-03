fe578a1e83d86e8492e8a437cbd6aab6ea0608d2
Milena Marques <84979688+milenamso@users.noreply.github.com>
2025-02-03 13:42:31 -0300

    Update Sistema.java

--- ClinicaMedica/src/main/Sistema.java
+++ ClinicaMedica/src/main/Sistema.java
@@ -1,12 +1,15 @@
 package main;
 import entities.*;
+
+import java.time.LocalDate;
+
 import javax.swing.JOptionPane;
 
 
 public class Sistema {
 	public static void main(String[] args) {
 		String optionString;
-		int optionBegin, optionPacienteBegin;
+		int optionBegin, optionPacienteBegin, optionMedicoBegin;
 		do {
 			optionString = JOptionPane.showInputDialog(null, "CLINICA MÉDICA\n\n"
 														   + "1 - PORTAL DO PACIENTE\n"
@@ -81,9 +84,73 @@ public class Sistema {
 				break;
 			}
 			case 2: {
-				
+				do {
+					optionString = JOptionPane.showInputDialog(null, "PORTAL DO MEDICO\n\n"
+																   + "1 - CADASTRAR MEDICO\n"
+																   + "2 - VER MEDICO\n"
+																   + "3 - ATUALIZAR MEDICO\n"
+																   + "4 - REMOVER MEDICO\n"
+																   + "5 - ACESSAR MEDICO\n"
+																   + "6 - VER TODOS OS MEDICOS\n"
+																   + "0 - VOLTAR");
+					optionMedicoBegin = Integer.parseInt(optionString);
+					
+					switch (optionMedicoBegin) {
+					case 1:
+						
+						String createNome = JOptionPane.showInputDialog("CADASTRAR MEDICO\n\nINSIRA O NOME");
+						String createCPF = JOptionPane.showInputDialog("CADASTRAR MEDICO\n\nINSIRA O CPF");
+						String createDataNascimento = JOptionPane.showInputDialog("CADASTRAR MEDICO\n\nINSIRA A DATA DE NASCIMENTO");
+						String createCRM = JOptionPane.showInputDialog("CADASTRAR MEDICO\n\nINSIRA O CRM");
+						String createEspecialidade = JOptionPane.showInputDialog("CADASTRAR MEDICO\n\nINSIRA A ESPECIALIDADE");
+						Medico novoMedico = new Medico(createNome, createCPF, null, createCRM, createEspecialidade);
+						boolean createVerificacao = RegistroCadastro.createMedico(novoMedico);
+						
+						if(createVerificacao) 
+							JOptionPane.showMessageDialog(null, "CADASTRO REALIZADO COM SUCESSO");
+						else 
+							JOptionPane.showMessageDialog(null, "CADASTRO NAO REALIZADO\nCPF FORNECIDO JA EXISTE NO SISTEMA");
+						
+						break;
+						
+					case 2:
+						String readCPF = JOptionPane.showInputDialog("VER MEDICO\n\nINFORME O CPF DO MEDICO");
+						RegistroCadastro.readMedico(readCPF);
+						break;
+					
+					case 3:
+						String updateCPF = JOptionPane.showInputDialog("ATUALIZAR MEDICO\n\nINFORME O CPF DO MEDICO");
+						RegistroCadastro.updateMedico(updateCPF);
+						break;		
+					case 4:
+						String removeCPF = JOptionPane.showInputDialog("REMOVER MEDICO\n\nINFORME O CPF DO MEDICO");
+						boolean removeVerificacao = RegistroCadastro.deleteMedico(removeCPF);
+						
+						if(removeVerificacao) 
+							JOptionPane.showMessageDialog(null, "REMOCAO REALIZADA COM SUCESSO"); 
+						else 
+							JOptionPane.showMessageDialog(null, "REMOCAO NAO REALIZADA\nCPF FORNECIDO NAO EXISTE NO SISTEMA");
+						
+						break;
+						
+					case 5:
+						String accessCPF = JOptionPane.showInputDialog("ACESSAR MEDICO\n\nINFORME O CPF DO MEDICO");
+						RegistroCadastro.accessMedico(accessCPF);
+						break;
+						
+					case 6:
+						RegistroCadastro.readAllMedicos();;
+						break;
+	
+					default:
+						if(optionMedicoBegin != 0) 
+							JOptionPane.showMessageDialog(null, "OPCAO INVALIDA");
+						break;
+					}
+				} while (optionMedicoBegin != 0);
 				break;
 			}
+
 			default:
 				if(optionBegin != 0) {
 					
